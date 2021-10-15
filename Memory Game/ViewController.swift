390e85b28b910b771acac5e4e3773745e9f94f29
Nikolai Saganenko <9952728@gmail.com>
2021-10-15 14:54:36 +0300

    Initial Commit

---
+++ Memory Game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Memory Game
+//
+//  Created by Nick Sagan on 15.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
