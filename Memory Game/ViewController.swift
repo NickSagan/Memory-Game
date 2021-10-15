180be0500260ad58b70fba6e75ef5dde8e0d6871
Nikolai Saganenko <9952728@gmail.com>
2021-10-15 15:38:31 +0300

    Make Flip counter function flipCards

--- Memory Game/ViewController.swift
+++ Memory Game/ViewController.swift
@@ -12,15 +12,35 @@ class ViewController: UIViewController {
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet var cardsCollection: [UIButton]!
     
+    var flipCounter = 0 {
+        didSet {
+            scoreLabel.text = "Flips: \(flipCounter)"
+        }
+    }
+    
+    var emojiArray = ["🐻", "🦊", "🐷", "🐻", "🦊", "🐷"]
+    
     @IBAction func cardButtonPressed(_ sender: UIButton) {
+        flipCounter += 1
+        
+        if let cardNumber = cardsCollection.firstIndex(of: sender) {
+            flipCard(withEmoji: emojiArray[cardNumber], on: sender)
+        }
+        
     }
     
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+    func flipCard(withEmoji emoji: String, on button: UIButton){
+        
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+        
     }
-
+    
 
 }
 
